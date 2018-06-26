86855727caf77ad8a3ebccec62ed9e96747ea080
Pierce <pierce.dejong45@gmail.com>
2018-06-26 09:23:17 -0600

    merge conflicts

--- app/src/main/java/com/spartancontrols/fbxconnect/ReportActivity.java
+++ app/src/main/java/com/spartancontrols/fbxconnect/ReportActivity.java
@@ -22,7 +22,6 @@ public class ReportActivity extends AppCompatActivity {
     static {
         // This crashed the program when trying to load
         //System.loadLibrary("opendnp3java");
-
     }
 
     // Store all of the params passed by the user from the previous screen
--- dnp3/cmake/settings.cmake
+++ dnp3/cmake/settings.cmake
@@ -4,3 +4,5 @@ include("${settings_cmake_dir}/inc/msvc.cmake")
 include("${settings_cmake_dir}/inc/posix.cmake")
 
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
+set(CMAKE_FIND_LIBRARY_PREFIXES "lib" "")
+set(CMAKE_FIND_LIBRARY_SUFFIXES ".dll.a" ".a" ".lib")
