d0721f2c4eab6d9536b1b9baf587dc57bcfaf78b
piercedejong <pierce.dejong45@gmail.com>
2018-07-17 14:55:48 -0600

    Removed old dnp3 files

--- dnp3/cmake/settings.cmake
+++
@@ -1,8 +0,0 @@
-get_filename_component(settings_cmake_dir "${CMAKE_CURRENT_LIST_FILE}" PATH)
-
-include("${settings_cmake_dir}/inc/msvc.cmake")
-include("${settings_cmake_dir}/inc/posix.cmake")
-
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
-set(CMAKE_FIND_LIBRARY_PREFIXES "lib" "")
-set(CMAKE_FIND_LIBRARY_SUFFIXES ".dll.a" ".a" ".lib")
